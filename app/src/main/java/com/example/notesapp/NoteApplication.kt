17fa713feb3aabd4c72fecd8dcb1abac5853e67e
Aleksandrina Kovachka <aleksandrina.kowachka@gmail.com>
2021-08-06 18:03:45 +0300

    Clear comments and unnecessary classes

--- app/src/main/java/com/example/notesapp/NoteApplication.kt
+++ app/src/main/java/com/example/notesapp/NoteApplication.kt
@@ -2,11 +2,7 @@ package com.example.notesapp
 
 import android.app.Application
 import com.example.notesapp.database.AppDatabase
-//import kotlinx.coroutines.CoroutineScope
-//import kotlinx.coroutines.SupervisorJob
 
 class NoteApplication : Application() {
-    //private val applicationScope = CoroutineScope(SupervisorJob())
-
     val database by lazy { AppDatabase.getDatabase(this) }
 }
